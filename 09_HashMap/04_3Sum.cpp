70dc1172c8a13ff78c321f3c646b876e82ec9388
ankitsainibkbiet <ankitsainibkbiet2022@gmail.com>
2025-04-29 08:55:10 +0530

    Data Structures and Algorithms

--- 09_HashMap/04_3Sum.cpp
+++ 09_HashMap/04_3Sum.cpp
@@ -17,7 +17,7 @@ void threeSumBruteForce(){
                 if(vec[i]+vec[j]+vec[k] == 0){
                     vector<int> temp = {vec[i], vec[j], vec[k]};  // to store a row in matrix
                     sort(temp.begin(), temp.end());
-                    if(s.find(temp) == s.end()){ // to check the duplicates and set can arrange the vector<int> but unordered_set need a hash function to arrange them which is not available for vector
+                    if(s.find(temp) == s.end()){ // to check the duplicates and also the set can arrange the vector<int> but unordered_set need a hash function to arrange them which is not available for vector
                         s.insert(temp);
                         matrix.push_back(temp);
                     }
@@ -42,7 +42,7 @@ void threeSumHashing(){
     set<vector<int>> s2; // to remove duplicates in pushback
 
     for(int i=0; i<n; i++){
-        s1.clear(); // to delete the no for every i to remove backward elements
+        s1.clear(); // to delete the current vec[i] and vec[j], which leads to false tripletes
         for(int j=i+1; j<n; j++){
             int target = -(vec[i] + vec[j]); 
             if(s1.find(target) != s1.end())
@@ -67,8 +67,14 @@ void threeSumHashing(){
     }
 }
 
+void threeSumTwoPointer(){
+    vector<int> vec = {-1,2,-2,0,1,-4};
+    int n = vec.size();
+    vector<vector<int>> matrix;
+}
+
 int main() {
     // threeSumBruteForce();   // O(n^3 * log(n))
-    // threeSumHashing();   // O(n^2 * log(n))
+    threeSumHashing();   // O(n^2 * log(n))
     return 0;
 }
