a20f5304bf9fdcecad30ed20b81526b5e5fde42c
ankitsainibkbiet <ankitsainibkbiet2022@gmail.com>
2025-05-01 20:05:19 +0530

    Data Structures and Algorithms

--- 09_HashMap/06_PlusOne.cpp
+++ 09_HashMap/06_PlusOne.cpp
@@ -1,22 +1,41 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
-vector<int> withNine(vector<int> vec, vector<int> ans, int n, int lastDigit){
+vector<int> withNine(vector<int> vec, vector<int> ans, int n){
     if(n == 1){
         ans.push_back(1);
         ans.push_back(0);
         return ans;
     }
+
+    for(int i=n-1; i>=0; i--){
+        if(vec[i] == 9){
+            ans.push_back(0);
+        }else if(vec[i] != 9 && vec[i+1] == 9){
+            ans.push_back(vec[i]+1);
+        }
+    }
+
+    bool insertOne = false;
+    if(vec[0] == 9) insertOne = true;
+
+    if(insertOne){
+        ans.push_back(1);
+    }
+
+    reverse(ans.begin(), ans.end());
+
+    return ans;
 }
 
-vector<int> withoutNine(vector<int> vec, vector<int> ans, int n, int lastDigit){
+vector<int> withoutNine(vector<int> vec, vector<int> ans, int n){
     for(int i=0; i<n; i++){
         if(i != n-1){
             ans.push_back(vec[i]);
         }else{
-            lastDigit = vec[n-1] + 1;
-            ans.push_back(lastDigit);
+            ans.push_back(vec[n-1] + 1);
         }
     }
 
@@ -24,14 +43,14 @@ vector<int> withoutNine(vector<int> vec, vector<int> ans, int n, int lastDigit){
 }
 
 int main() {
-    vector<int> vec = {9};
+    vector<int> vec = {1,9,9,1};
     vector<int> ans;
-    int n = vec.size(), lastDigit;
+    int n = vec.size();
 
     if(vec[n-1] == 9){
-        ans = withNine(vec, ans, n, lastDigit);
+        ans = withNine(vec, ans, n);
     }else{
-        ans = withoutNine(vec, ans, n, lastDigit);
+        ans = withoutNine(vec, ans, n);
     }
 
     for(int val : ans){
