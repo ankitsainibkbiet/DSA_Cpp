11b3421b0bf20d28e35347d5df555b583ed9fc9c
ankitsainibkbiet <ankitsainibkbiet2022@gmail.com>
2025-05-02 07:18:30 +0530

    Data Structures and Algorithms

---
+++ 09_HashMap/07_2Sum_II.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+    vector<int> numbers = {2,7,11,15};
+    vector<int> ans;
+    int n = numbers.size(), start = 0, end = n-1, target = 9;
+
+    while(start < end){
+        int sum = 0;
+        sum = numbers[start] + numbers[end];
+        if(sum < target){
+            start++;
+        }else if(sum > target){
+            end--;
+        }else{
+            ans.push_back(start);
+            ans.push_back(end);
+            break;
+        }
+    }
+
+    for(int val:ans){
+        cout<<val<<" ";
+    }
+
+    return 0;
+}
