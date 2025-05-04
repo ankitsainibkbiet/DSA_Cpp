11ae963bd086b571f909f3b5dab29b6096285a09
ankitsainibkbiet <ankitsainibkbiet2022@gmail.com>
2025-05-04 11:10:27 +0530

    Data Structures and Algorithms

---
+++ 10_Recursion/01_Factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+long long factorial(int n){
+    if(n == 1 || n == 0){
+        return 1;
+    }
+    return factorial(n-1) * n;
+}
+
+int main() {
+    int n = 50;
+    cout<<factorial(n);
+    return 0;
+}
