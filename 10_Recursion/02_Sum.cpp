9dafb70ed432e9060cf5b00d731e7e6bdfa07207
ankitsainibkbiet <ankitsainibkbiet2022@gmail.com>
2025-05-04 11:39:22 +0530

    Data Structures and Algorithms

---
+++ 10_Recursion/02_Sum.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int sum(int n){
+    if(n == 1){
+        return 1;
+    }
+    return sum(n-1) + n;
+}
+
+int main(){
+    int n = 4;
+    cout<<sum(n);
+    return 0;
+}
