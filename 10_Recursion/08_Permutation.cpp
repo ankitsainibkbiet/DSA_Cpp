57d96f4746edab51203856f765bd87e8800038c8
ankitsainibkbiet <ankitsainibkbiet2022@gmail.com>
2025-05-06 20:32:32 +0530

    Data Structures and Algorithms

--- 10_Recursion/08_Permutation.cpp
+++ 10_Recursion/08_Permutation.cpp
@@ -15,18 +15,38 @@ void permutation(vector<int>& nums, vector<vector<int>>& ans, int n, int index){
     }
 }
 
+void stringPermutation(string& str, int n, vector<string>& ans, int index){
+    if(index == n){
+        ans.push_back(str);
+        return;
+    }
+
+    for(int i=index; i<n; i++){
+        swap(str[index], str[i]);
+        stringPermutation(str, n, ans, index+1);
+        swap(str[index], str[i]);
+    }
+}
+
 int main(){
     vector<int> nums = {1,2,3};
     int n = nums.size();
     vector<vector<int>> ans;
+    // permutation(nums, ans, n, 0);
+    // for(auto val: ans){
+    //     for(auto key: val){
+    //         cout<<key<<" ";
+    //     }
+    //     cout<<endl;
+    // }
 
-    permutation(nums, ans, n, 0);
-
-    for(auto val: ans){
-        for(auto key: val){
-            cout<<key<<" ";
-        }
-        cout<<endl;
+    string str = "abc";
+    int n2 = str.length();
+    vector<string> ans2;
+    stringPermutation(str, n2, ans2, 0);
+    for(auto val: ans2){
+        cout<<val<<endl;
     }
+
     return 0;
 }
