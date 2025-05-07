4ffbef9a96c35f35e48b23af6c8c7334957b8f6d
ankitsainibkbiet <ankitsainibkbiet2022@gmail.com>
2025-05-07 19:18:26 +0530

    Data Structures and Algorithms

--- 10_Recursion/09_N-Queens.cpp
+++ 10_Recursion/09_N-Queens.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 using namespace std;
 
-bool isSafe(vector<string>& board, vector<vector<string>>& ans, int n, int row, int column){
+bool isSafe(vector<string>& board, int n, int row, int column){
     for(int i=0; i<n; i++){  // Row
         if(board[i][column] == 'Q'){
             return false;
@@ -37,7 +37,7 @@ void nQueens(vector<string>& board, vector<vector<string>>& ans, int n, int row)
     }
 
     for(int j=0; j<n; j++){
-        if(isSafe(board, ans, n, row, j)){
+        if(isSafe(board, n, row, j)){
             board[row][j] = 'Q';
             nQueens(board, ans, n, row+1);
             board[row][j] = '.';
@@ -47,7 +47,7 @@ void nQueens(vector<string>& board, vector<vector<string>>& ans, int n, int row)
 
 int main(){
     int n = 4;
-    vector<string> board(n, string(n, '.'));
+    vector<string> board(n, string(n, '.')); // string is also a 1D array
     vector<vector<string>> ans;
 
     nQueens(board, ans, n, 0);
