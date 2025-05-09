fef9037c6f5af1a8cf3ffe5133437b16218951f1
ankitsainibkbiet <ankitsainibkbiet2022@gmail.com>
2025-05-09 23:04:48 +0530

    Data Structures and Algorithms

--- 05_String/14_LongestCommonPrefix.cpp
+++ 05_String/14_LongestCommonPrefix.cpp
@@ -4,27 +4,29 @@
 using namespace std;
 
 int main(){
-    vector<string> str = {"flower", "flow", "flight"};
+    vector<string> str = {"flower", "flow", "fliwht"};
     string first = str[0];
     int count = INT_MAX;
     
     for(int i=1; i<str.size(); i++){
         string compare = str[i];
         int j=0;
-        int length = -1;
+        int length = 0;  // -1 is dangourse for compiler
         while(j < first.length() && j < compare.length()){
             if(first[j] == compare[j]){
                 length++;
+            }else{
+                break;  // use for continuous match only
             }
             j++;
         }
         count = min(count, length);
     }
 
-    if(count == -1){
+    if(count == 0){
         cout<<"-1";
     }else{
-        cout<<first.substr(0, count+1);
+        cout<<first.substr(0, count);
     }
 
     return 0;
