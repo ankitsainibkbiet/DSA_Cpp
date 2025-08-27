113917b8bedf9495728113faef06752325afb0e8
ankitsainibkbiet <ankitsainibkbiet2022@gmail.com>
2025-08-27 14:01:51 +0530

    DSA

--- 05_String/15_LongestSubstring.cpp
+++ 05_String/15_LongestSubstring.cpp
@@ -4,18 +4,19 @@ using namespace std;
 
 int main(){
     string s = "bvbf";
+    vector<int> freq(256, -1);
     int maxLength = 0, left = 0, right = 0;
-    vector<int> freq(256, 0);  // it can handle anything 
-
-    while(right<s.length()){
-        if(freq[s[right]] == 0){
-            freq[s[right]]++;
-            maxLength = max(maxLength, right-left+1);
-            right++;
-        }else{
-            freq[s[left]]--;
-            left++;
+   
+    while(right < s.length()){
+        if(freq[s[right]] != -1){
+            if(freq[s[right]] >= left){
+                left = freq[s[right]] + 1;
+            }
         }
+        int length = right - left + 1;
+        maxLength = max(maxLength, length);
+        freq[s[right]] = right;
+        right++;
     }
 
     cout<<maxLength;
